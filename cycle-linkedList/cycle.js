fe035fb2b4dd3afd27c3fbcc34189bae1d11ea8f
Karma Drukpa <karmadrukpa11@gmail.com>
2020-05-01 15:51:24 -0700

    rectify cycle solution

--- cycle-linkedList/cycle.js
+++ cycle-linkedList/cycle.js
@@ -16,11 +16,12 @@ const hasCycle = function (head) {
 	let slow = head;
 	let fast = head;
 	while (fast && fast.next) {
+		slow = slow.next;
+		fast = fast.next.next;
+
 		if (slow === fast) {
 			return true;
 		}
-		slow = slow.next;
-		fast = fast.next.next;
 	}
 	return false;
 };
